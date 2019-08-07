96022fbf7c7fb23fbd14e4b35609c634b399e2e1
aliang <504268721@qq.com>
2019-08-07 15:44:26 +0800

    登入controller

--- src/main/java/com/value/controller/LoginController.java
+++ src/main/java/com/value/controller/LoginController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
     public Object getUserByValue() {
-        return "";
+        return "ni123213";
     }
 }
